4c5a4d461eef019d07f85a7d008759e895d3c5a6
Sunil <s_agasti@apple.com>
2026-04-21 17:08:32 -0500

    remove default logs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['child_process'],
+  devIndicators: false,
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
