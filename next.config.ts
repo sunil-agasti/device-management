34371ac9d251e33d8750d6fd0167f7157f448b08
Sunil <s_agasti@apple.com>
2026-04-30 12:50:09 -0500

    push

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: import.meta.dirname,
+  },
   serverExternalPackages: ['child_process'],
   devIndicators: false,
   poweredByHeader: false,
